5ae85fbf63bdb90cbd759a407845a1bd95582afc
Sonia364 <c0872364@mylambton.ca>
2022-11-30 02:44:28 +0000

    Implemented the list view functionality

--- app/src/main/java/com/sonia/myapplication/MainActivity.java
+++ app/src/main/java/com/sonia/myapplication/MainActivity.java
@@ -3,6 +3,13 @@ package com.sonia.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText text = findViewById(R.id.newText);
+        Button addBtn = findViewById(R.id.addBtn);
+
+        ArrayList<String> listData = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                listData
+        );
+
+        ListView list = findViewById(R.id.listTest);
+        list.setAdapter(adapter);
+
+        addBtn.setOnClickListener(
+                v->{
+                    String textValue = text.getText().toString();
+                    if(textValue.isEmpty()){
+                        Toast.makeText(this, "Please enter the text!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        listData.add(textValue);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(this, "New value is added!", Toast.LENGTH_SHORT).show();
+                        text.setText("");
+                    }
+
+                }
+        );
+        //adapter.notifyDataSetChanged();
     }
 }
